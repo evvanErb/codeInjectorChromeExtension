d3ab17960f02c7ee801575bf6aeeef233e4aace4
evvanErb <evvan@protonmail.com>
2021-02-13 10:24:05 -0500

    inject edited

--- inject.js
+++ inject.js
@@ -1,16 +1,29 @@
 // this is the code which will be injected into a given page...
-function codeToInject() {
-    window.__popcorn__test__ = true;
-}
 
-function embed(fn) {
-    const script = document.createElement("script");
-    script.text = `(${fn.toString()})();`;
-    document.documentElement.appendChild(script);
+function embed(choice, code) {
+    if(choice.toLowerCase() == "js") {
+        const script = document.createElement("script");
+        script.text = `${code}`;
+        document.documentElement.appendChild(script);
+    }
+    else if(choice.toLowerCase() == "html") {
+        const html = document.createElement("div");
+        html.innerHTML = `${code}`;
+        document.documentElement.appendChild(html);
+    }
+    else if(choice.toLowerCase() == "css") {
+        const css = document.createElement("style");
+        css.type = 'text/css';
+        if (css.styleSheet)
+                css.styleSheet.cssText = `${code}`;
+        else
+            css.appendChild(document.createTextNode(`${code}`));
+        document.getElementsByTagName("head")[0].appendChild(css);
+    }
 }
-
-embed(codeToInject);
 
 (function() {
-    embed(codeToInject);
+    var choice = prompt("Please enter type of code to inject at bottom of webpage CSS, HTML, or JS", "");
+    var code = prompt("Enter code", "");
+    embed(choice, code);
 })();
